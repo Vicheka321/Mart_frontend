5e2277aa468808f6e0a856c7a86b74164e92bfee
Vicheka321 <vichekalong754@gmail.com>
2026-08-15 01:46:41 +0700

    update

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -18,9 +18,11 @@ import '../models/my_orders_model.dart';
 import '../models/profile_model.dart';
 
 class ApiService {
-  final String baseUrl = 'https://glutton-heat-trifle.ngrok-free.dev/api';
+  // final String baseUrl = 'https://glutton-heat-trifle.ngrok-free.dev/api';
   // final String baseUrl = 'https://daritamart.site/api';
   // final String baseUrl = 'http://10.0.2.2:8000/api';
+  final String baseUrl = 'https://accomplished-respect-production-3a13.up.railway.app/api';
+
 
   // ==============Products=================
 
